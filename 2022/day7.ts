b677bd6b940c55f01a73be747d2650b157747224
Mohamed Mayla <mohamed.mayla@gmail.com>
2022-12-11 17:02:00 +0100

    solve day 7

--- 2022/day7.ts
+++ 2022/day7.ts
@@ -24,19 +24,49 @@ interface FileNode extends BaseNode {
 
 type Node = DirectoryNode | FileNode;
 
-const findDirectory = (
+const findSubDirectory = (
   name: string,
   root: DirectoryNode
 ): DirectoryNode | undefined => {
-  if (root.name === name) {
-    return root;
-  }
   const found = root.children.find((child) => child.name === name);
   if (found && found.type === "directory") {
     return found;
   }
 
   return undefined;
+};
+
+type Command = "cd" | "ls";
+
+const handleCommand = (
+  command: Command,
+  arg: string,
+  root: DirectoryNode,
+  currentDir: DirectoryNode
+): DirectoryNode => {
+  if (command !== "cd") {
+    return currentDir;
+  }
+
+  if (arg === "..") {
+    if (currentDir.parent) {
+      return currentDir.parent;
+    } else {
+      console.error("cd to nonexisting parent");
+    }
+  } else {
+    if (arg === "/") {
+      return root;
+    }
+    const found = findSubDirectory(arg, currentDir);
+    if (found) {
+      return found;
+    } else {
+      console.error(`Directory not found ${arg}`);
+    }
+  }
+
+  return currentDir;
 };
 
 const createTree = (lines: string[]): DirectoryNode => {
@@ -54,23 +84,7 @@ const createTree = (lines: string[]): DirectoryNode => {
 
     if (parsedLine[0] === "$") {
       const [_, command, arg] = parsedLine;
-      if (command === "cd") {
-        if (arg === "..") {
-          if (currentDir.parent) {
-            currentDir = currentDir.parent;
-          } else {
-            console.error("cd to nonexisting parent");
-          }
-          return;
-        }
-        const found = findDirectory(arg, currentDir);
-        if (found) {
-          currentDir = found;
-        } else {
-          console.error(`Directory not found ${arg}`);
-        }
-      }
-
+      currentDir = handleCommand(command as Command, arg, tree, currentDir);
       return;
     }
 
@@ -98,8 +112,10 @@ const createTree = (lines: string[]): DirectoryNode => {
   return tree;
 };
 
-const MAX_DIR_SIZE = 100000;
-let sizeSum = 0;
+const MAX_DIR_SIZE = 10_0000;
+const TOTAL_DISK_SIZE = 70_000_000;
+const REQUIRED_DISK_SIZE = 30_000_000;
+let totalSizeLessThanMax = 0;
 
 const calculateSizesRecursively = (node: DirectoryNode): number => {
   node.size = 0;
@@ -107,7 +123,7 @@ const calculateSizesRecursively = (node: DirectoryNode): number => {
     if (child.type === "directory") {
       const dirSize = calculateSizesRecursively(child);
       if (dirSize < MAX_DIR_SIZE) {
-        sizeSum += dirSize;
+        totalSizeLessThanMax += dirSize;
       }
       node.size += dirSize;
     } else {
@@ -118,7 +134,38 @@ const calculateSizesRecursively = (node: DirectoryNode): number => {
   return node.size;
 };
 
+const findMinDirectoryAbove = (
+  threshold: number,
+  root: DirectoryNode
+): number => {
+  let minSize = Infinity;
+  const stack: DirectoryNode[] = [root];
+  while (stack.length > 0) {
+    const node = stack.pop();
+    if (node && node.size && node.size >= threshold) {
+      if (node.size < minSize) {
+        minSize = node.size;
+      }
+      node.children.forEach((child) => {
+        if (child.type === "directory") {
+          stack.push(child);
+        }
+      });
+    }
+  }
+
+  return minSize;
+};
+
 const root = createTree(lines);
 const rootSize = calculateSizesRecursively(root);
+const unusedDiskSpace = TOTAL_DISK_SIZE - rootSize;
+const neededDiskSpace = REQUIRED_DISK_SIZE - unusedDiskSpace;
 
-console.log(root, rootSize, sizeSum);
+const minDirectorySize = findMinDirectoryAbove(neededDiskSpace, root);
+
+console.log("Root size:", rootSize);
+console.log("Unused disk space:", unusedDiskSpace);
+console.log("Needed disk space:", neededDiskSpace);
+console.log("Part 1 result:", totalSizeLessThanMax);
+console.log("Part 2 result:", minDirectorySize);
